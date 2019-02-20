d33706bbcbaae989a6cfbdf4c7e05a1e20c23543
David Lavelle <davidlavelle1@hotmail.com>
2019-02-20 20:27:49 +0000

    trying to make a move

--- src/client.py
+++ src/client.py
@@ -1,17 +1,53 @@
 #!/usr/bin/env python3
+import sys
+import time
 import requests
 
 SERVER_URL = "http://127.0.0.1:5000"
 CONNECT_URL = f"{SERVER_URL}/connect"
+STATE_URL = f"{SERVER_URL}/state"
+MOVE_URL = f"{SERVER_URL}/move"
+WAIT_INTERVAL = 2
 
 
 def connect():
     name = input("Enter your name: ")
     response = requests.post(CONNECT_URL, json={"name": name})
-    print(response.json())
+    if response.status_code != requests.codes.created:
+        print(response.json().get("message"))
+        sys.exit()
+
+    return name
+
+
+def display_board(board):
+    print(board)
+
+
+def make_move(column, name):
+    data = {
+        "column": column,
+        "name": name,
+    }
+    response = requests.patch(MOVE_URL, json=data)
+
+def get_game_state(name):
+    response = requests.get(STATE_URL)
+    response_data = response.json()
+    turn = response_data["turn"]
+    if turn == name:
+        display_board(response_data["board"])
+        # TODO: Validatioin
+        column = input(f"It's your turn {name}, please enter column (1 - 9): ")
+        make_move(column, name)
+    else:
+        # TODO: Handle turn = None (different message)
+        print(f"Waiting on player {turn} . . .")
+        time.sleep(WAIT_INTERVAL)
 
 
 if __name__ == "__main__":
     # TODO: try/finally for SIG_TERM => DELETE /connect
-    #while True:
-    connect()
+    name = connect()
+    while True:
+        get_game_state(name)
--- src/server.py
+++ src/server.py
@@ -1,11 +1,11 @@
-from flask import Flask, request, jsonify, json
+from flask import Flask, request, json, jsonify
 
 app = Flask(__name__)
 
 
 # TODO: Do i need a lock here?
 class Game(object):
-    state = None
+    turn = None
     players = []
     EMPTY = ""
     X = "x"
@@ -13,11 +13,10 @@ class Game(object):
 
     @classmethod
     def new_player(cls, name):
-        no_of_players = len(cls.players)
-        if no_of_players == 0:
-            cls.start_new_game()
-        if no_of_players < 2:
+        if len(cls.players) < 2:
             cls.players.append(name)
+        if len(cls.players) == 1:
+            cls.start_new_game()
         else:
             return False
         return True
@@ -25,6 +24,27 @@ class Game(object):
     @classmethod
     def start_new_game(cls):
         cls.board = [[cls.EMPTY for i in range(6)] for j in range(9)]
+        cls.turn = cls.players[0]
+
+
+    @classmethod
+    def make_move(cls, column, name):
+        if name != cls.turn:
+            return
+        move = int(column) - 1
+        # TODO: Do move in board
+        cls.toggle_turn(name)
+
+    @classmethod
+    def toggle_turn(cls, just_moved):
+        if just_moved == cls.players[0]:
+            try:
+                cls.turn = cls.players[1]
+            except IndexError:
+                # other player may not have joined yet
+                cls.turn = None
+        else:
+            cls.turn = cls.players[0]
 
 
 # TODO: A delete method to end game
@@ -33,13 +53,14 @@ def join():
     name = request.json.get("name")
     if Game.new_player(name):
         response = json.dumps({
+            "message": "OK",
             "board": Game.board,
-            "players": Game.players,
+            "turn": Game.turn,
         })
         status = 201
     else:
         response = json.dumps({
-            "error":"This game is already full, try again later."
+            "message": "This game is already full, try again later."
         })
         status = 403
     response = app.response_class(
@@ -48,3 +69,17 @@ def join():
         mimetype='application/json'
     )
     return response
+
+
+@app.route("/state", methods=["GET"])
+def state():
+    return jsonify({"turn": Game.turn, "board": Game.board})
+
+
+@app.route("/move", methods=["PATCH"])
+def move():
+    # TODO: Column may be full?
+    column = request.json.get("column")
+    name = request.json.get("name")
+    Game.make_move(column, name)
+    return "OK"
